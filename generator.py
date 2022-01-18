02fbe4c95c541e881f4c522d2f3075799125695f
hagamablabla <hagamablabla@gmail.com>
2022-01-17 19:43:27 -0500

    empty resource worlds

--- generator.py
+++ generator.py
@@ -1,9 +1,10 @@
 import json
+import math
 import random
 
 def main():
     print('GURPS Infinite Worlds Generator')
-    print('v0.0.4\n')
+    print('v0.0.5\n')
     print('1 - Generate new world automatically')
     print('2 - Generate new world step-by-step')
     print('3 - Change settings (TO-DO)')
@@ -127,6 +128,7 @@ def selectQuantum(automatic, results):
 def selectWorldType(automatic, results):
     # World Type
     if (not automatic):
+        print('\nWorld Type')
         print('1 - Empty')
         print('2 - Echo/Parallel')
         print('3 - Echo')
@@ -167,10 +169,73 @@ def selectWorldType(automatic, results):
         if (results['quantum'] == 6):
             worldType = ['Echo', 'Echo', 'Parallel', 'Parallel', 'Parallel', 'Parallel'][option - 1]
         else:
-            worldType = ['Echo', 'Echo', 'Echo', 'Parallel', 'Parallel', 'Parallel'][option - 1]
+            worldType = ['Echo', 'Parallel', 'Parallel', 'Parallel', 'Parallel', 'Parallel'][option - 1]
     
     print('World Type: ' + worldType)
     results['worldType'] = worldType
+
+    if (worldType == 'Empty'):
+        emptyWorldGen(automatic, results)
+
+
+def emptyWorldGen(automatic, results):
+    # Empty World Type
+    if (not automatic):
+        print('\nEmpty Worlds')
+        print('1 - Resource exploitation')
+        print('2 - Homeline colony or colonies')
+        print('3 - Disaster world')
+
+        while True:
+            option = input()
+            try:
+                option = int(option)
+            except ValueError:
+                continue
+
+            if (option == 1):
+                break
+            elif (option == 2):
+                emptyType = 'Homeline colony or colonies'
+                break
+            elif (option == 3):
+                emptyType = 'Disaster world'
+                break
+            elif (option == ''):
+                option = random.randint(1, 6)
+                break
+            else:
+                continue
+    else:
+        option = random.randint(1, 6)
+
+    if (not ('emptyType' in locals())):
+        emptyType = ['Resource exploitation', 'Resource exploitation', 'Resource exploitation', 'Resource exploitation', 'Homeline colony or colonies', 'Disaster world'][option - 1]
+    
+    print('Empty World Type: ' + emptyType)
+    results['emptyType'] = emptyType
+
+    if (emptyType == 'Resource exploitation'):
+        resourceWorldGen(automatic, results)
+
+def resourceWorldGen(automatic, results):
+    resourcePrice = {'Bauxite':20, 'Beryllium':600000, 'Chromium':500, 'Cobalt':50000, 'Gold':8800000, 'Platinum':12800000, 'Silver':440000, 'Titanium':8000, 'Uranium':14000}
+    resourceProbability = [10, 1, 4, 2, 15, 25, 40, 50, 10]
+
+    # Resources
+    if (not automatic):
+        print('\nWealth')
+        print('Generated automatically')
+    
+    numMines = math.ceil(random.lognormvariate(2, 1) * random.lognormvariate(2, 1))
+    resourceMines = random.choices(list(resourcePrice.keys()), resourceProbability, k = numMines)
+    mineCount = {}
+    for resource in list(resourcePrice.keys()):
+        mineCount[resource] = resourceMines.count(resource)
+        print(resource, ': ', mineCount[resource], end = ' ')
+    
+    print(mineCount)
+    results['mines'] = mineCount
 
 if __name__ == '__main__':
     main()
